5582c26e286e76fc20fcb6cb03be1d30da4fa46f
Tim <voncurleone@gmail.com>
2023-01-11 19:49:09 -0500

    Created skeleton for MyHashTable.scala.

--- src/main/scala/com/arraysandstrings/MyHashTable.scala
+++ src/main/scala/com/arraysandstrings/MyHashTable.scala
@@ -1,5 +1,27 @@
 package com.arraysandstrings
 
-class MyHashTable {
+object MyHashTable {
+  def apply[K,V](pairs: (K, V)*): MyHashTable[K,V] =
+    val table = new MyHashTable[K,V]()
+    for(pair <- pairs) {
+      table.add(pair)
+    }
+    table
+}
 
+class MyHashTable[K,V]() {
+  private var array = new Array[List[(K, V)]](10)
+  private val growthRatio = .75
+  private var elems = 0
+
+  private def rehash(): Unit = ???
+  def add(elem: (K, V)): Unit = ???
+  def remove(key: K): Unit = ???
+  def get(key: K): Option[V] = ???
+  def keySet: Set[K] = ???
+  def contains(key: K): Boolean = ???
+
+  override def equals(obj: Any): Boolean = super.equals(obj)
+
+  override def toString: String = super.toString
 }
