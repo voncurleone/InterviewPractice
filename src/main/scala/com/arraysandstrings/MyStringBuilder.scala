87dbcb9c677796bf21669e95ff437328e02973a7
Tim <voncurleone@gmail.com>
2023-01-12 18:11:22 -0500

    Implemented MyStringBuilder.

--- src/main/scala/com/arraysandstrings/MyStringBuilder.scala
+++ src/main/scala/com/arraysandstrings/MyStringBuilder.scala
@@ -1,5 +1,19 @@
 package com.arraysandstrings
 
-class MyStringBuilder {
+object MyStringBuilder {
+  def apply(): MyStringBuilder = new MyStringBuilder()
+}
+class MyStringBuilder() {
+  val arrayList: MyArrayList[String] = MyArrayList()
 
+  def add(s: String): Unit =
+    arrayList.append(s)
+
+  override def toString: String =
+    var ret = ""
+
+    for(i <- 0 until arrayList.length) {
+      ret = ret + arrayList(i)
+    }
+    ret
 }
