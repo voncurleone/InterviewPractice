e5c5ea9a231fb12a391ef58e2d393a08a7611180
Tim <voncurleone@gmail.com>
2023-01-16 17:46:36 -0500

    Added tests for rotateMatrix.

--- src/test/scala/com/arraysandstrings/ExercisesSuite.scala
+++ src/test/scala/com/arraysandstrings/ExercisesSuite.scala
@@ -103,4 +103,34 @@ class ExercisesSuite extends AnyWordSpec{
       stringCompression("tOObbbY") should === ("tO2b3Y")
     }
   }
+
+  "rotateMatrix" must {
+    "rotate a square matrix" in {
+      val in = Array(
+        Array(1,2,3),
+        Array(4,5,6),
+        Array(7,8,9)
+      )
+      val exp = Array(
+        Array(7,4,1),
+        Array(8,5,2),
+        Array(9,6,3)
+      )
+      rotateMatrix(in) should === (exp)
+
+      val in2 = Array(
+        Array(1,2,3,4),
+        Array(5,6,7,8),
+        Array(9,10,11,12),
+        Array(13,14,15,16)
+      )
+      val exp2 = Array(
+        Array(13,9,5,1),
+        Array(14,10,6,2),
+        Array(15,11,7,3),
+        Array(16,12,8,4)
+      )
+      rotateMatrix(in2) should === (exp2)
+    }
+  }
 }
