88381477586636bb5581ecbd6d8d83267a537b9f
Tim <voncurleone@gmail.com>
2023-01-18 17:27:08 -0500

    Implemented some functionality of MyLinkedList.

--- src/main/scala/com/linkedlist/MyLinkedList.scala
+++ src/main/scala/com/linkedlist/MyLinkedList.scala
@@ -1,20 +1,51 @@
 package com.linkedlist
 
-sealed trait MyNode[+A]
-case class Node[A](var elem: A, var next: MyNode[A] = End) extends MyNode[A]
-case object End extends MyNode[Nothing]
+import scala.annotation.{tailrec, targetName}
+
+sealed trait MyNode
+case class Node[A](var elem: A, var next: MyNode = End) extends MyNode
+case object End extends MyNode
 
 class MyLinkedList[A]() {
-  private var head: MyNode[A] = End
+  private var head: MyNode = End
   private var size: Int = 0
   
   def length: Int = size
-  def append(elem: A): Unit = ???
-  def insert(elem: A): Unit = ???
+  def append(elem: A): Unit =
+    @tailrec
+    def loop(node: MyNode): Unit = node match
+      case End =>
+        head = Node(elem)
+        size += 1
+      case n @ Node(_, End) =>
+        n.next = Node(elem, End)
+        size += 1
+      case Node(_, next) =>
+        loop(next)
+
+    loop(head)
+
+  def prepend(elem: A): Unit = ???
+  def insert(elem: A, index: Int): Unit = ???
   def removeHead(): Unit = ???
   def removeTail(): Unit = ???
   def remove(index: Int): Unit = ???
   def isEmpty: Boolean = head == End
+
+  override def equals(obj: Any): Boolean =
+    @tailrec
+    def loop(n1: MyNode, n2: MyNode): Boolean = n1 match
+      case Node(elem, next) => n2 match
+        case Node(e, n) =>
+          if elem != e then false
+          else loop(next, n)
+      case End => true
+
+    obj match
+      case list: MyLinkedList[A] =>
+        if list.size != size then false
+        else loop(list.head, head)
+      case _ => false
 
   //exercises
   def removeDupes(): Unit = ???
@@ -26,7 +57,14 @@ class MyLinkedList[A]() {
 }
 
 object MyLinkedList {
-  def apply[A](elems: A*): MyLinkedList[A] = ???
+  def apply[A](elems: A*): MyLinkedList[A] =
+    val list = new MyLinkedList[A]()
+
+    for(elem <- elems) {
+      list.append(elem)
+    }
+
+    list
 }
 
 
