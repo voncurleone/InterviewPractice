a86ccb09021603db3c186825e1d53a6c065d47f4
Tim <voncurleone@gmail.com>
2023-01-17 19:09:41 -0500

    Added more and implemented length and isEmpty.

--- src/main/scala/com/linkedlist/MyLinkedList.scala
+++ src/main/scala/com/linkedlist/MyLinkedList.scala
@@ -1,21 +1,33 @@
 package com.linkedlist
 
 sealed trait MyNode[+A]
-case class Node[A](elem: A, next: MyNode[A] = End) extends MyNode[A]
+case class Node[A](var elem: A, next: MyNode[A] = End) extends MyNode[A]
 case object End extends MyNode[Nothing]
 
 class MyLinkedList[A]() {
-  var head: MyNode[A] = End
-  var tail: MyNode[A] = End
+  private var head: MyNode[A] = End
+  private var size: Int = 0
   
-  def length: Int = ???
+  def length: Int = size
   def append(elem: A): Unit = ???
   def insert(elem: A): Unit = ???
   def removeHead(): Unit = ???
   def removeTail(): Unit = ???
   def remove(index: Int): Unit = ???
-  def isEmpty: Boolean = ???
+  def isEmpty: Boolean = head == End
+  def appendNode(node: MyNode[A]): Unit = ???
+
+  //exercises
+  def removeDupes(): Unit = ???
   def apply(index: Int): A = ???
+  def deleteMiddle(): Unit = ???
+  def partition(): Unit = ???
+  def sum(list: MyLinkedList[A]): MyLinkedList[A] = ???
+  def palindrome: Boolean = ???
+  //def intersection(list: MyLinkedList[A]): MyNode[A] = ???
+  //def loopDetection: MyNode[A] = ???
+
+
 }
 
 object MyLinkedList {
