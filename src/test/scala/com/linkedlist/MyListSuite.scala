726f8d8961d854c785c11eede53a84fada99bed9
Tim <voncurleone@gmail.com>
2023-01-25 17:46:56 -0500

    Added more tests for MyList.

--- src/test/scala/com/linkedlist/MyListSuite.scala
+++ src/test/scala/com/linkedlist/MyListSuite.scala
@@ -4,8 +4,10 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers.should
 
 val empty = MyList.empty[Int]
+val one = MyList(1)
 val two = MyList(1,2)
 val three = MyList(1,2,3)
+val four = MyList(1,2,3,4)
 
 class MyListSuite extends AnyWordSpec {
   "MyList" should {
@@ -80,6 +82,15 @@ class MyListSuite extends AnyWordSpec {
       "return the MyList that is being appended" in {
         val list = empty.append(two)
         list should === (two)
+      }
+
+      "toString should return the correct string" in {
+        empty.toString should === ("MyList()")
+      }
+
+      "flatten on empty should return empty" in {
+        val emptyL = MyList.empty[MyList[MyList[Int]]]
+        MyList.flatten(emptyL) should === (Empty)
       }
     }
 
@@ -159,10 +170,141 @@ class MyListSuite extends AnyWordSpec {
         three.take(0) should ===(Empty)
         three.take(1) should === (MyList(1))
         three.take(2) should === (MyList(1,2))
+        three.take(2).length should === (2)
         three.take(3) should ===(three)
       }
 
+      "drop throws exception if n < 0" in {
+        intercept[IllegalArgumentException] {
+          three.drop(-1)
+        }
+      }
 
+      "drop returns Empty if n > than size" in {
+        three.drop(42) should === (Empty)
+      }
+
+      "drop returns returns the list - the first n elements" in {
+        three.drop(0) should === (three)
+        three.drop(1) should === (MyList(2,3))
+        three.drop(1).length should === (2)
+        three.drop(2) should === (MyList(3))
+        three.drop(3) should === (Empty)
+      }
+
+      "length should provide the number of elems in the list" in {
+        two.length should === (2)
+        three.length should === (3)
+      }
+
+      "reverse should return the list with the opposite order of elems" in {
+        two.reverse should === (MyList(2,1))
+        three.reverse should === (MyList(3,2,1))
+      }
+
+      "contains should return false if the list does not contain the elem" in {
+        two.contains(3) should === (false)
+        three.contains(4) should === (false)
+      }
+
+      "contains should return true if the list contains the elem" in {
+        two.contains(1) should === (true)
+        three.contains(2) should === (true)
+      }
+
+      "toString should return the correct string to represent the list" in {
+        three.toString should === ("MyList(1,2,3)")
+        two.toString should === ("MyList(1,2)")
+      }
+
+      "flatten should flatten a non empty List" in {
+        val list = MyList(MyList(1,2), Empty, MyList(3,4), Empty, MyList(5))
+        MyList.flatten(list) should === (MyList(1,2,3,4,5))
+
+        val list2 = MyList(MyList(1,2), MyList(3,4), MyList(5))
+        MyList.flatten(list2) should === (MyList(1,2,3,4,5))
+      }
+    }
+
+    "exercises" should {
+      val noDupes = MyList(1,2,3,4,5,6,7,8,9)
+      val dupes = MyList(1,1,2,3,4,5,5,6,7,7,8,9,9)
+      "remove dupes returns the same list if there are no dupes" in {
+        MyList.removeDupes(noDupes) should === (noDupes)
+      }
+
+      "remove dupes should remove all duplicate elems" in {
+        MyList.removeDupes(dupes) should === (noDupes)
+      }
+
+      "delete middle returns Empty if the list is empty" in {
+        MyList.deleteMiddle(empty) should === (Empty)
+      }
+
+      "delete middle removes the one elem in a list with an odd length" in {
+        MyList.deleteMiddle(three) should === (MyList(1,2))
+      }
+
+      "delete middle removes the middle 2 elements" in {
+        MyList.deleteMiddle(two) should === (Empty)
+        MyList.deleteMiddle(four) should === (MyList(1,4))
+      }
+
+      val pList = MyList(5,8,1,3,2,9)
+      "partition on empty will return empty" in {
+        MyList.partition(4, empty) should === (Empty)
+        MyList.partition(-4, empty) should === (Empty)
+      }
+
+      "partition with n greater than list should reverse list" in {
+        MyList.partition(10, pList) should === (MyList(9,2,3,1,8,5))
+      }
+
+      "partition with n less than list elems should return list " in {
+        MyList.partition(0, pList) should === (pList)
+      }
+
+      "partition should work on non edge cases" in {
+        MyList.partition(3, pList) should === (MyList(2,1,5,8,3,9))
+        MyList.partition(5, pList) should === (MyList(2,3,1,5,8,9))
+        MyList.partition(9, pList) should === (MyList(2,3,1,8,5,9))
+      }
+
+      "sum should return empty if both lists are empty" in {
+        MyList.sum(empty, empty) should === (Empty)
+      }
+
+      "sum should return list that is not empty if one is empty" in {
+        MyList.sum(empty, three) should === (three)
+        MyList.sum(two, empty) should === (two)
+      }
+
+      "should calculate the sum properly" in {
+        val l1 = MyList(7,1,6)
+        val l2 = MyList(5,9,5)
+
+        MyList.sum(l1, l2) should === (MyList(2,1,2,1))
+
+        val l3 = MyList(1,2)
+        val l4 = MyList(1)
+
+        MyList.sum(l3, l4) should === (MyList(2,2))
+      }
+
+      "palindrome should return true if it is empty or contains only one elem" in {
+        MyList.palindrome(empty) should === (true)
+        MyList.palindrome(one) should === (true)
+      }
+
+      "palindrome should return true for even and odd length palindrome" in {
+        MyList.palindrome(MyList(0,1,2,1,0)) should === (true)
+        MyList.palindrome(MyList(0,1,2,2,1,0)) should === (true)
+      }
+
+      "palindrome should return true for long middles" in {
+        MyList.palindrome(MyList(0,1,2,2,2,1,0)) should ===(true)
+        MyList.palindrome(MyList(0,1,2,2,2,2,1,0)) should ===(true)
+      }
     }
   }
 }
