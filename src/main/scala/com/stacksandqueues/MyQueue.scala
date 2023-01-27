1f93de34577e4754c0e45a2c4d36a2b836098f09
Tim <voncurleone@gmail.com>
2023-01-26 19:08:25 -0500

    Implemented MyQueue.

--- src/main/scala/com/stacksandqueues/MyQueue.scala
+++ src/main/scala/com/stacksandqueues/MyQueue.scala
@@ -1,20 +1,41 @@
 package com.stacksandqueues
 
+import com.stacksandqueues.MyQueue.fillFront
+
 class MyQueue[A] {
   var front: List[A] = Nil
   var end: List[A] = Nil
 
   @throws[IllegalStateException]("stack is empty")
-  def pop: A = ???
+  def pop: A =
+    if isEmpty then throw new IllegalStateException("queue is empty")
+    if front.isEmpty then
+      fillFront(this)
+      val ret = front.head
+      front = front.tail
+      ret
+    else
+      val ret = front.head
+      front = front.tail
+      ret
 
-  def push(elem: A): Unit = ???
+  def push(elem: A): Unit = end = elem :: end
 
   @throws[IllegalStateException]("stack is empty")
-  def peek: A = ???
+  def peek: A =
+    if isEmpty then throw new IllegalStateException("queue is empty")
+    if front.isEmpty then
+      fillFront(this)
+      front.head
+    else front.head
 
-  def isEmpty: Boolean = ???
+  def isEmpty: Boolean = front.isEmpty && end.isEmpty
 }
 
 object MyQueue {
   def apply[A](): MyQueue[A] = new MyQueue[A]
+
+  private def fillFront[A](q: MyQueue[A]): Unit =
+    q.front = q.end.reverse
+    q.end = Nil
 }
