d7d3a11a61a2a8102e8fc0505da0577ff6c96927
Tim <voncurleone@gmail.com>
2023-01-31 19:33:03 -0500

    Implemented ListStackSuite.

--- src/test/scala/com/stacksandqueues/ListSetSuite.scala
+++ src/test/scala/com/stacksandqueues/ListSetSuite.scala
@@ -102,7 +102,7 @@ class ListSetSuite extends AnyWordSpec {
 
         stack.push(1)
         stack.length should === (1)
-        stack.popAt(1) should === (1)
+        stack.popAt(0) should === (1)
         stack.isEmpty should === (true)
 
         stack.push(2)
@@ -115,7 +115,53 @@ class ListSetSuite extends AnyWordSpec {
         }
       }
       
-      
+      "should return and remove the element in the stack specified in the listStack" in {
+        val stack = ListStack[Int]()
+
+        for(i <- 0 until 15) {
+          stack.push(i)
+        }
+
+        stack.length should === (2)
+        stack.popAt(0) should === (9)
+        stack.popAt(0) should === (8)
+
+        stack.popAt(1) should === (14)
+        stack.popAt(1) should === (13)
+        stack.popAt(1) should === (12)
+        stack.popAt(1) should === (11)
+        stack.popAt(1) should === (10)
+
+        stack.length should === (1)
+      }
+    }
+
+    "implement length" should {
+      "return 0 for an empty listStack" in {
+        val stack = ListStack[Int]()
+
+        stack.length should === (0)
+      }
+
+      "return the correct length for a listStack with multiple stacks" in {
+        val stack = ListStack[Int]()
+
+        for(i <- 0 until 25) {
+          stack.push(i)
+        }
+
+        stack.length should === (3)
+      }
+    }
+
+    "implement isEmpty" should {
+      "return true when stack is empty and false when it contains elements" in {
+        val stack = ListStack[Int]()
+
+        stack.isEmpty should === (true)
+        stack.push(1)
+        stack.isEmpty should === (false)
+      }
     }
   }
 }
