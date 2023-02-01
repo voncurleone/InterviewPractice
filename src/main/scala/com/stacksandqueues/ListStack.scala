041be8f33979885be2a4ec2970b092c5182e9868
Tim <voncurleone@gmail.com>
2023-01-31 19:32:48 -0500

    Implemented ListStack.

--- src/main/scala/com/stacksandqueues/ListStack.scala
+++ src/main/scala/com/stacksandqueues/ListStack.scala
@@ -4,23 +4,42 @@ import com.arraysandstrings.MyArrayList
 
 class ListStack[A] {
   private val array: MyArrayList[MyStack[A]] = MyArrayList()
-  private val maxSize = 10
-  private var stackSize = 0
 
   @throws[IllegalStateException]("stack is empty")
-  def pop: A = ???
+  def pop: A =
+    if isEmpty then throw new IllegalStateException()
+    else
+      val ret = array(array.length - 1).pop
+      if array(array.length - 1).size == 0 then
+        array.remove(array.length - 1)
+        ret
+      else ret
 
   @throws[IllegalArgumentException]("stack is empty")
-  def peek: A = ???
+  def peek: A =
+    if isEmpty then throw new IllegalStateException()
+    else array(array.length - 1).peek
 
-  def push(elem: A): Unit = ???
+  def push(elem: A): Unit =
+    if isEmpty || array(array.length - 1).size == 10 then
+      array.append(MyStack())
+      array(array.length - 1).push(elem)
+    else
+      array(array.length - 1).push(elem)
 
   @throws[IllegalArgumentException]("index out of bounds")
-  def popAt(index: Int): A = ???
+  def popAt(index: Int): A =
+    if index < 0 || index > array.length - 1 then throw new IllegalArgumentException()
+    else
+      val ret = array(index).pop
+      if array(index).size == 0 then
+        array.remove(index)
+        ret
+      else ret
 
-  def length: Int = ???
+  def length: Int = array.length
 
-  def isEmpty: Boolean = ???
+  def isEmpty: Boolean = if array.length == 0 then true else false
 }
 
 object ListStack {
