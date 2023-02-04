c28fba418e807c666c38789c6edade38ffa02d7a
Tim <voncurleone@gmail.com>
2023-02-03 20:58:11 -0500

    added implementation for depth.

--- src/main/scala/com/treesandgraphs/MyBST.scala
+++ src/main/scala/com/treesandgraphs/MyBST.scala
@@ -4,8 +4,10 @@ sealed trait MyBST[+A] {
   def insert[B >: A : Ordering](elem: B): MyBST[B] = ???
   def remove[B >: A](elem: B): MyBST[B] = ???
 
-  def depth: Int = ???
+  def depth: Int
+  def balanceFactor: Int = ???
   def balance: MyBST[A] = ???
+  def isBalanced: Boolean = ???
   def rotateRight: MyBST[A] = ???
   def rotateLeft: MyBST[A] = ???
   def rotateRightLeft: MyBST[A] = ???
@@ -15,12 +17,18 @@ sealed trait MyBST[+A] {
   def preOrder: String = ???
   def postOrder: String = ???
 
-  override def equals(obj: Any): Boolean = ???
+  override def equals(obj: Any): Boolean = ??? //might not need this
   override def toString: String = inOrder
 }
-case class Branch[A](elem: A, left: MyBST[A] = Leaf, right: MyBST[A] = Leaf) extends MyBST[A]
-case object Leaf extends MyBST[Nothing]
+case class Branch[A](elem: A, var left: MyBST[A] = Leaf, var right: MyBST[A] = Leaf) extends MyBST[A] {
+  def depth =
+    if left.depth > right.depth then left.depth + 1
+    else right.depth + 1
+}
+case object Leaf extends MyBST[Nothing] {
+  def depth: Int = 0
+}
 
 object MyBST {
-  def apply[A: Ordering](): MyBST[A] = ???
+  def apply[A: Ordering](): MyBST[A] = Leaf
 }
